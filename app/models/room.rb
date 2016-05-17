26b4b8698490d70a85a79cf45825a398bfa4460b
pjhjohn <pjhjohn@gmail.com>
2016-05-17 23:31:29 +0000

    Multiple Image Uploader working

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,9 @@
 class Room < ActiveRecord::Base
   belongs_to :user
+  mount_uploader :representative, RoomImageUploader
+  mount_uploader :image1, RoomImageUploader
+  mount_uploader :image2, RoomImageUploader
+  mount_uploader :image3, RoomImageUploader
+  mount_uploader :image4, RoomImageUploader
+  mount_uploader :image5, RoomImageUploader
 end

e6868deb61d617b169b25aacb43135e8d17d0b06
pjhjohn <pjhjohn@gmail.com>
2016-05-19 20:53:40 +0000

    admin/index implemented

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,18 +4,25 @@ class AdminController < ApplicationController
   def index
     if not current_user.is_admin
       redirect_to "/"
+    else
+      @user_count = User.count
+      @room_count = Room.count
     end
   end
 
   def users
     if not current_user.is_admin
       redirect_to "/"
+    else
+      @users = User.all
     end
   end
 
   def rooms
     if not current_user.is_admin
       redirect_to "/"
+    else
+      @rooms = Room.all
     end
   end
 end
